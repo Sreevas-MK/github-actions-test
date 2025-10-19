dba3b76c9e2273ac483e97bf4e04be1f16988f32
Sreevas-MK <sreevasmk@gmail.com>
2025-10-19 00:02:41 +0000

    Flask app: Dockerfile with env variables, app.py & requirements.txt - First commit

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM python:3.11-alpine
+
+ENV APP_USER flask_user
+
+ENV APP_DIRECTORY /var/flaskapp
+
+RUN mkdir $APP_DIRECTORY
+
+RUN adduser -h $APP_DIRECTORY -s /bin/sh -D -H $APP_USER
+
+WORKDIR $APP_DIRECTORY
+
+COPY . .
+
+RUN pip install -r requirements.txt
+
+RUN chown -R $APP_USER:$APP_USER $APP_DIRECTORY
+
+EXPOSE 5000
+
+USER $APP_USER
+
+ENTRYPOINT ["python"]
+
+CMD ["app.py"]
+
---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import os
+
+# Create an instance of the Flask application
+app = Flask(__name__)
+
+# Define a route for the root URL ("/")
+@app.route("/")
+def hello_world():
+    return '<h1><center>Flask Application Version1</center></h1>'
+
+# Run the application if the script is executed directly
+if __name__ == "__main__":
+
+    app_port = os.getenv('APP_PORT', 8080)
+    # Use port mentioned in env or else use 8080
+    app.run(host='0.0.0.0',port=app_port,debug=True)
